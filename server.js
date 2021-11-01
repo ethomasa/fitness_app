f3ea02b1b10ea03a3d0fb129231dcde44987db9d
Eby <ethomasa@gmail.com>
2021-11-01 10:50:23 -0500

    server and app file

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes.js")(app);
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}!`);
+});

0119374bb6d7d2ba00f5d22e994c9df862d9e69e
Eby <ethomasa@gmail.com>
2021-11-01 11:08:18 -0500

    api and html routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,39 @@
+const db = require("../models");
+
+module.exports = function(app) {
+    // workouts page
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+    })
+    // range page
+    app.get("/api/workouts/range", ({}, res) => {
+      db.Workout.find({}).then((dbWorkout) => {
+        res.json(dbWorkout);
+      }).catch(err => {
+        res.status(500).json(err);
+      });
+    });
+    // completed workouts
+    app.post("/api/workouts/", (req, res) => {
+        db.Workout.create(req.body).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+            res.status(500).json(err);
+          });
+      });
+      //  update workouts 
+      app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+          { _id: req.params.id }, { exercises: req.body }
+        ).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+          res.status(500).json(err);
+        });
+    });
+};

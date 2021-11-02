673fa6fbef7a1907f25abab1c95c73658be756a4
Eby <ethomasa@gmail.com>
2021-11-01 21:31:02 -0500

    remote db fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -11,8 +11,9 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
+console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  
   useNewUrlParser: true,
   useFindAndModify: false
 });
